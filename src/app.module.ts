5c01e208ca5b8c38d953c621a03d72ede582ad2e
22051107 <22051107@kiit.ac.in>
2025-04-09 19:43:43 +0530

    final file modified

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, BooksModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      process.env.MONGO_URI || 'mongodb://localhost:27017/bookstore',
+    ),
+    AuthModule,
+    BooksModule,
+  ],
 })
 export class AppModule {}
--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { BooksController } from './books.controller';
+import { BooksService } from './books.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Book, BookSchema } from './schemas/book.schema';
+import { JwtStrategy } from '../auth/jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Book.name, schema: BookSchema }]),
+    PassportModule,
+  ],
+  controllers: [BooksController],
+  providers: [BooksService, JwtStrategy],
+})
 export class BooksModule {}
