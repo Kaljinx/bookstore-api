864e3a2efb45586fbe274d86cc42033277ee883c
22051107 <22051107@kiit.ac.in>
2025-04-09 23:20:30 +0530

    final file modified

---
+++ src/books/dto/update-book.dto.ts
@@ -0,0 +1,27 @@
+import { IsString, IsOptional, IsNumber, IsDate } from 'class-validator';
+
+export class UpdateBookDto {
+  @IsOptional()
+  @IsString()
+  title?: string;
+
+  @IsOptional()
+  @IsString()
+  author?: string;
+
+  @IsOptional()
+  @IsString()
+  category?: string;
+
+  @IsOptional()
+  @IsNumber()
+  price?: number;
+
+  @IsOptional()
+  @IsNumber()
+  rating?: number;
+
+  @IsOptional()
+  @IsDate()
+  published_date?: Date;
+}
