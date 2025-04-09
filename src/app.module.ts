dce3525f5fba4d56af1115c27948de58e5fe2427
22051107 <22051107@kiit.ac.in>
2025-04-09 17:13:32 +0530

    all Module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, AuthModule, BooksModule],
   controllers: [AppController],
   providers: [AppService],
 })
